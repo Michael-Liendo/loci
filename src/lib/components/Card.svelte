974cc8a837f55e13a01c968b7e874bcd8c2d0120
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-01 14:09:15 -0400

    chore: create card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	let customClassName: string | null = null;
+	export { customClassName as class };
+
+	let className = classNames(customClassName, 'bg-white rounded-lg p-4 w-full');
+</script>
+
+<div class={className}>
+	<slot />
+</div>
